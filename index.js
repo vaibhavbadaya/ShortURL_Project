50b9a3fddae15e851938399cfc4dacf9992ee26d
Vaibhav Badaya <153429925+vaibhavbadaya@users.noreply.github.com>
2026-01-03 22:35:55 +0530

    added files modules

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const app = express();
 const {connectMongoDB} = require('./config');
 const urlRouter = require('./routes/url');
 const { URL } = require('./models/url');
+const path = require('path');
+const staticRouter = require('./routes/staticRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
 connectMongoDB('mongodb://localhost:27017/shorturldb')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Failed to connect to MongoDB', err));
 
 app.use('/api/url', urlRouter);
+app.use('/', staticRouter);
+
+app.set("views",path.resolve("./views"));
 
 app.get('/api/:shortUrl', async (req, res) => {
     try {
@@ -31,7 +37,11 @@ app.get('/api/:shortUrl', async (req, res) => {
     }
 });
     
-
+app.get('/test', async (req, res) => {
+    const allUrls = await URL.find({})
+    return res.render("home", { 
+        urls: allUrls })
+});
 
 app.listen(8001, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
