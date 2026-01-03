3a2722395c21ed49b4d231350be2fe5a176bc484
Vaibhav Badaya <153429925+vaibhavbadaya@users.noreply.github.com>
2026-01-03 23:13:29 +0530

    fixed some bugs

--- index.js
+++ index.js
@@ -9,17 +9,24 @@ const staticRouter = require('./routes/staticRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
 
 connectMongoDB('mongodb://localhost:27017/shorturldb')
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Failed to connect to MongoDB', err));
 
+app.set('view engine', 'ejs');
+app.set("views",path.resolve("./views"));
+
 app.use('/url', urlRouter);
 app.use('/', staticRouter);
 
-app.set("views",path.resolve("./views"));
+app.get('/test', async (req, res) => {
+    const allUrls = await URL.find({})
+    return res.render("home", { 
+        urls: allUrls })
+});
 
+// Catch-all redirect route (must be after all other routes)
 app.get('/api/:shortUrl', async (req, res) => {
     try {
         const shortUrl = req.params.shortUrl;
@@ -35,12 +42,6 @@ app.get('/api/:shortUrl', async (req, res) => {
         console.error(err);
         return res.status(500).send('Server error');
     }
-});
-    
-app.get('/test', async (req, res) => {
-    const allUrls = await URL.find({})
-    return res.render("home", { 
-        urls: allUrls })
 });
 
 app.listen(8001, () => {
